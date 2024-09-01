276a910adf1e6eb770a6d41775ad0f13b6e2a9c2
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-01 18:41:48 -0300

    followMe socialNetworks

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,73 +1,56 @@
 import React from "react";
-import { SiBlueprint } from "react-icons/si";
+import {
+  SiBlueprint,
+  SiLinkedin,
+  SiInstagram,
+  SiGithub,
+  SiReddit,
+} from "react-icons/si";
 
 function header() {
   return (
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-3">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <SiBlueprint class="w-8 h-8 dark:text-white" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+    <header className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <SiBlueprint className="w-8 h-8 dark:text-white" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             3DCostify
           </span>
         </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
 
-        <div class="flex sm:justify-center space-x-6">
-          <a href="#" class="text-white-500 hover:text-white-900">
-            <svg
-              class="h-7"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
+        <div className="flex sm:justify-center px-8 border-l-2 border-r-2 items-center h-9 cursor-pointer group">
+          <span className="uppercase absolute opacity-1 group-hover:opacity-0 transition-opacity duration-300 pointer-events-none">
+            Follow me
+          </span>
+          <nav className="flex sm:justify-center space-x-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <a
+              href="#"
+              className="text-white-500 hover:text-white-900 hover:scale-125 transition-transform duration-300"
             >
-              <path
-                fill-rule="evenodd"
-                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a href="#" class="text-white-500 hover:text-white-900">
-            <svg
-              class="h-7"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
+              <SiLinkedin className="w-6 h-6" />
+            </a>
+            <a
+              href="#"
+              className="text-white-500 hover:text-white-900 hover:scale-125 transition-transform duration-300"
             >
-              <path
-                fill-rule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
+              <SiInstagram className="w-6 h-6" />
+            </a>
+            <a
+              href="#"
+              className="text-white-500 hover:text-white-900 hover:scale-125 transition-transform duration-300"
+            >
+              <SiGithub className="w-6 h-6" />
+            </a>
+            <a
+              href="#"
+              className="text-white-500 hover:text-white-900 hover:scale-125 transition-transform duration-300"
+            >
+              <SiReddit className="w-6 h-6" />
+            </a>
+          </nav>
         </div>
       </div>
-    </nav>
+    </header>
   );
 }
 
