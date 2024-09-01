9ed354ca8f37aa23a5ade1c86584496c4717bbf3
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-01 12:30:05 -0300

    FirstCommit!

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold underline text-center">FIRST COMMIT!!!</h1>
+    </div>
+  );
+}
+
+export default App;
