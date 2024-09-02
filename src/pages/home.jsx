b7cdc042daf96bd556a4c36f75e73d4873a481e7
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-01 22:54:37 -0300

    centerTitle

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,14 @@ import "./styles/home.css";
 function home() {
   return (
     <div className="bg-gray-900 h-screen">
-      <div className="container max-w-full"></div>
+      <div className="container max-w-full">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-8xl text-white font-bold">3DCostify</h1>
+          <p className="text-white text-2xl mt-2">
+            3D printing cost calculator
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
