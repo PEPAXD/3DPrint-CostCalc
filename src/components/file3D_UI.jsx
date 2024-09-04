af06af1ec1652aa4fc7085eb9e1a53a26fbf7d89
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-04 00:31:48 -0300

    add new 3dFile button, and style flex text center

--- src/components/file3D_UI.jsx
+++ src/components/file3D_UI.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function file3D_UI({ file }) {
+  const [uploadedFile, setUploadedFile] = useState(file);
+
+  let FileChange = (e) => {
+    setUploadedFile(e.target.files[0]);
+  };
+
   return (
     <>
-      <h2 className="absolute top-16 left-1/2 transform -translate-x-1/2 text-white text-4xl underline underline-offset-4">
-        {file.name}
-      </h2>
+      <div className="w-full h-full mt-32">
+        <h2 className="flex justify-center text-white text-4xl underline underline-offset-4">
+          {uploadedFile.name.length > 40
+            ? uploadedFile.name.substring(0, 40)
+            : uploadedFile.name}
+        </h2>
+      </div>
+      <button
+        onClick={() => document.getElementById("fileInput").click()}
+        className="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded"
+      >
+        Add New 3DFile
+      </button>
+      <input
+        id="fileInput"
+        type="file"
+        className="hidden"
+        onChange={FileChange}
+      />
     </>
   );
 }
