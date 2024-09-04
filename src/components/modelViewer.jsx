6f528006afc89f98dd2798e46e288419b6f826e1
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-03 22:54:03 -0300

    disable <input3d> when uploadFile

--- src/components/modelViewer.jsx
+++ src/components/modelViewer.jsx
@@ -16,7 +16,7 @@ function modelViewer() {
   return (
     <div className="flex h-full w-full p-3">
       <div className="relative w-full rounded-xl border-dashed border-2 border-white-900 flex justify-center items-center">
-        <Input3D onFileUpload={handleFileUpload} />
+        {!uploadedFile && <Input3D onFileUpload={handleFileUpload} />}
       </div>
     </div>
   );
