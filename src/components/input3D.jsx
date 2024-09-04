1b671ae88420cba18ba71be5d6683991a90426bf
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-03 21:50:08 -0300

    inputfile

--- src/components/input3D.jsx
+++ src/components/input3D.jsx
@@ -4,7 +4,13 @@ import { TbFile3D } from "react-icons/tb";
 
 function input3D() {
   return (
-    <div className="absolute">
+    <div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-600 opacity-20">
+        <div className="h-full  bg-gray-600 flex justify-center items-center">
+          <input type="file" className="h-full w-full opacity-0" name="" />
+        </div>
+      </div>
+
       <div className="flex flex-col items-center">
         <TbFile3D className="text-[10rem] text-gray-400 stroke-1" />
 
--- src/components/modelViewer.jsx
+++ src/components/modelViewer.jsx
@@ -9,12 +9,6 @@ function modelViewer() {
   return (
     <div className="flex h-full w-full p-3">
       <div className="relative w-full rounded-xl border-dashed border-2 border-white-900 flex justify-center items-center">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-600 opacity-20">
-          <div className="h-full  bg-gray-600 flex justify-center items-center">
-            <input type="file" className="h-full w-full opacity-0" name="" />
-          </div>
-        </div>
-
         <Input3D />
       </div>
     </div>
