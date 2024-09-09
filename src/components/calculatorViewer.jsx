3aece83db68584fa076c3a2cb4daceaa604fed80
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-09 16:28:11 -0300

    priceCardCalculator

--- src/components/calculatorViewer.jsx
+++ src/components/calculatorViewer.jsx
@@ -3,12 +3,11 @@ import React from "react";
 function calculatorViewer() {
   return (
     <div className="flex h-full w-full p-3">
-      <div className="relative w-full border-2 border-white-900 flex justify-center items-center bg-blue-300">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-75"></div>
-
-        <div className="w-full h-full mt-32 z-10">
-          <h3 className="flex pl-8 text-white text-2xl">CostCalculator</h3>
-          <hr className="mx-4" />
+      <div className="relative w-full border-2 border-white-900 flex justify-center items-center">
+        <div className="drop-shadow-xl h-full w-full overflow-hidden  bg-[#3d3c3d]">
+          <div className="absolute flex items-center justify-center text-white z-[1] opacity-90 inset-0.5 bg-[#002244]">
+            Smart 3D Print Price Checker
+          </div>
         </div>
       </div>
     </div>
